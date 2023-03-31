0b7ddd0d3df88a6d1665183076acf07879df6efa
Vassilis Margonis <basilis.math@gmail.com>
2023-04-01 00:39:39 +0300

    refactor - fixed gitignore

---
+++ lib/models/losses.py
@@ -0,0 +1,255 @@
+from keras import backend as K
+import tensorflow as tf
+import math
+
+
+def kl_divergence(
+    mean: tf.Tensor,
+    log_var: tf.Tensor,
+) -> tf.Tensor:
+    """Computes batch KL divergence.
+
+    Parameters
+    ----------
+    mean : tf.Tensor
+        Encoder mean, tensor of size (batch_size, latent_dim).
+    log_var : tf.Tensor
+        Encoder log variance, tensor of size (batch_size, latent_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        batch KL divergence, tensor of size ().
+    """
+
+    kl = K.square(mean) + K.exp(log_var) - log_var - 1  # shape:(?,L)
+    sample_kl = 0.5 * K.sum(kl, axis=-1)  # shape:(?,)
+
+    return K.mean(sample_kl, axis=0)  # shape:()
+
+
+def bernoulli_loss(
+    original: tf.Tensor,
+    reconstruction: tf.Tensor,
+    is_binary_input: bool,
+) -> tf.Tensor:
+    """Computes batch cross-entropy loss for multi-bernoulli decoder.
+
+    Parameters
+    ----------
+    original : tf.Tensor
+        VAE input, tensor of size (batch_size, initial_dim).
+    reconstruction : tf.Tensor
+        VAE output, tensor of size (batch_size, initial_dim).
+    is_binary_input : boolean
+        incicates whether pixels are binary.
+
+    Returns
+    -------
+    tf.Tensor
+        Batch cross entropy loss, tensor of size ().
+    """
+
+    # if input is not binary, lower bound in cross entropy is not zero
+    if is_binary_input:
+        xent_lower_bound = 0
+
+    else:
+        original_clipped = K.clip(original, 1e-7, 1 - 1e-7)
+        xent_lower_bound = -K.sum(
+            original_clipped * K.log(original_clipped),
+            axis=-1,
+        )  # shape:(?,)
+
+    reconstruction_clipped = K.clip(reconstruction, 1e-7, 1 - 1e-7)
+    # cross-entropy
+    a = original * K.log(reconstruction_clipped)
+    b = (1 - original) * K.log(1 - reconstruction_clipped)
+    xent = -K.sum(a + b, axis=-1)
+    sample_xent = xent - xent_lower_bound  # shape:(?,)
+
+    return K.mean(sample_xent, axis=0)
+
+
+def gaussian_loss(
+    original: tf.Tensor,
+    reconstruction: tf.Tensor,
+) -> tf.Tensor:
+    """Computes batch L2 loss for Gaussian decoder.
+
+    Parameters
+    ----------
+    original : tf.Tensor
+        VAE input, tensor of size (batch_size, initial_dim).
+    reconstruction : tf.Tensor
+        VAE output, tensor of size (batch_size, initial_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        Batch L2 loss, tensor of size ().
+    """
+
+    loss = K.sum(K.square(original - reconstruction), axis=-1)  # shape:(?,)
+    return K.mean(loss, axis=0)
+
+
+def compute_covariance_mean(mean: tf.Tensor) -> tf.Tensor:
+    """Computes the batch covariance matrix of means for DIP-VAE I.
+    Uses cov(means) = E[means^T * means] - E[means]^T * E[means].
+
+    Parameters
+    ----------
+    mean : tf.Tensor
+        Encoder mean, tensor of size (batch_size, latent_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        Covariance of encoder mean, tensor of size (latent_dim, latent_dim).
+    """
+
+    batch_size = K.cast(K.shape(mean)[0], dtype="float32")
+
+    # Compute a = E[means^T * means], shape:(L,L)
+    a = K.dot(K.transpose(mean), mean) / batch_size
+
+    # Compute b = E[means]^T * E[means], shape:(L,L)
+    expected_means = K.mean(mean, axis=0, keepdims=True)  # shape:(1,L)
+    b = K.dot(K.transpose(expected_means), expected_means)  # shape:(L,L)
+
+    return a - b
+
+
+def compute_covariance_z(
+    mean: tf.Tensor,
+    log_var: tf.Tensor,
+) -> tf.Tensor:
+    """Computes the batch covariance matrix of Gaussian samples z
+    for DIP-VAE II. Uses Cov_q(z)(z) = E[Cov] + Cov(means).
+
+    Parameters
+    ----------
+    mean : tf.Tensor
+        Encoder mean, tensor of size (batch_size, latent_dim).
+    log_var : tf.Tensor
+        Encoder log variance, tensor of size (batch_size, latent_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        batch covariance matrix of z, tensor of size (latent_dim, latent_dim).
+    """
+
+    # Cov(means)
+    covariance_means = compute_covariance_mean(mean)  # shape:(L,L)
+    # E[Cov]
+    mean_sigma_squared = K.mean(K.exp(log_var), axis=0)  # shape:(L,)
+    expected_covariance = tf.linalg.diag(mean_sigma_squared)  # shape:(L,L)
+
+    return expected_covariance + covariance_means
+
+
+def dip_vae_regularizer(
+    cov_matrix: tf.Tensor,
+    lambda_off_diag: float,
+    lambda_diag: float,
+) -> tf.Tensor:
+    """Compute regularizers for DIP-VAE I & II.
+
+    Parameters
+    ----------
+    cov_matrix : tf.Tensor
+        Covariance Matrix to regularize, size = (latent_dim, latent_dim)
+    lambda_off_diag : float
+        Weight of penalty for off-diagonal elements.
+    lambda_diag : float
+        Weight of penalty for diagonal elements.
+
+    Returns
+    -------
+    tf.Tensor
+        DIP regularizer, tensor of size ().
+    """
+
+    cov_matrix_diagonal = tf.linalg.diag_part(cov_matrix)  # shape:(L,)
+    cov_matrix_off_diagonal = cov_matrix - tf.linalg.diag(cov_matrix_diagonal)
+
+    off_diag_penalty = lambda_off_diag * K.sum(cov_matrix_off_diagonal**2)
+    diag_penalty = lambda_diag * K.sum((cov_matrix_diagonal - 1) ** 2)
+
+    return off_diag_penalty + diag_penalty
+
+
+def gaussian_log_density(
+    z: tf.Tensor,
+    mean: tf.Tensor,
+    log_var: tf.Tensor,
+) -> tf.Tensor:
+    """Computes element-wise Gaussian log density.
+    Uses log(q(z)) = -0.5 * (log(2pi) + log(sigma^2) + (z-mu)^2 * sigma^(-2))
+
+    Parameters
+    ----------
+    z : tf.Tensor
+        gaussian samples, tensor of size (batch_size, latent_dim).
+    mean : tf.Tensor
+        Encoder mean, tensor of size (batch_size, latent_dim).
+    log_var : tf.Tensor
+        Encoder log variance, tensor of size (batch_size, latent_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        log density, tensor of size (batch_size, latent_dim).
+    """
+
+    log_2pi = K.log(2.0 * tf.constant(math.pi))
+    inverse_vars = K.exp(-log_var)
+    return -0.5 * (log_2pi + log_var + inverse_vars * (z - mean) * (z - mean))
+
+
+def total_correlation(
+    z: tf.Tensor,
+    mean: tf.Tensor,
+    log_var: tf.Tensor,
+) -> tf.Tensor:
+    """Estimation of total correlation on a batch.
+    We need to compute the expectation over a batch of:
+    E_j [log(q(z(x_j))) - log(prod_l q(z(x_j)_l))].
+    Constants are ignored as they do not matter for the minimization.
+
+    Parameters
+    ----------
+    z : tf.Tensor
+        gaussian samples, tensor of size (batch_size, latent_dim).
+    mean : tf.Tensor
+        Encoder mean, tensor of size (batch_size, latent_dim).
+    log_var : tf.Tensor
+        Encoder log variance, tensor of size (batch_size, latent_dim).
+
+    Returns
+    -------
+    tf.Tensor
+        Total correlation estimated on a batch.
+    """
+
+    # Compute log(q(z(x_j)|x_i)) for every sample in the batch, which is a
+    # tensor of size (batch_size, batch_size, latent_dim). In the following
+    # comments, (batch_size, batch_size, latent_dim) are indexed by (j, i, l).
+    log_qz_prob = gaussian_log_density(
+        tf.expand_dims(z, 1), tf.expand_dims(mean, 0), tf.expand_dims(log_var, 0)
+    )
+    # Compute log prod_l p(z(x_j)_l) =
+    # sum_l(log(sum_i(q(z(z_j)_l|x_i))) + constant)
+    # for each sample in the batch, which is a vector of size (batch_size,).
+    log_qz_product = tf.reduce_sum(
+        tf.reduce_logsumexp(log_qz_prob, axis=1, keepdims=False), axis=1, keepdims=False
+    )
+    # Compute log(q(z(x_j))) as log(sum_i(q(z(x_j)|x_i))) + constant =
+    # log(sum_i(prod_l q(z(x_j)_l|x_i))) + constant.
+    log_qz = tf.reduce_logsumexp(
+        tf.reduce_sum(log_qz_prob, axis=2, keepdims=False), axis=1, keepdims=False
+    )
+
+    return tf.reduce_mean(log_qz - log_qz_product)
---
+++ lib/models/vae.py
@@ -0,0 +1,138 @@
+from keras.models import Model
+from lib.models.layers import reparameterize, dense_encoder, dense_decoder
+from lib.models.losses import (
+    kl_divergence,
+    total_correlation,
+    compute_covariance_z,
+    compute_covariance_mean,
+    dip_vae_regularizer,
+)
+from lib.models.utils import add_optimizer, get_reconstruction_loss
+
+
+class BaseVAE:
+    """Base VAE architecture"""
+
+    def __init__(self, config):
+        self._in, self.mean, self.log_var, self.encoder = dense_encoder(config)
+        self.reparam = reparameterize(config)
+        self.decoder = dense_decoder(config)
+        self._z = self.reparam(self.encoder(self._in))
+        self._out = self.decoder(self.reparam(self.encoder(self._in)))
+        self.vae = Model(self._in, self._out)
+
+
+class BetaVAE(BaseVAE):
+    def __init__(self, config, params):
+        super().__init__(config)
+
+        # string representation
+        self.str_repr = (
+            f"betavae"
+            f"_L{config['latent_dim']}"
+            f"_beta{params['beta']}"
+            f"_wseed{config['weight_seed']}"
+        )
+
+        # Add Loss
+        self.vae.add_loss(self.beta_vae_loss(config, params))
+        self.vae.compile(optimizer=add_optimizer(config))
+
+    def beta_vae_loss(self, config, params):
+        """BETAVae Batch Loss:
+        beta * KL divergence + reconstruction loss
+        """
+
+        kl = kl_divergence(self.mean, self.log_var)
+
+        reconstruction_loss = get_reconstruction_loss(
+            self._in,
+            self._out,
+            config,
+        )
+
+        return params["beta"] * kl + reconstruction_loss
+
+
+class TCVAE(BaseVAE):
+    def __init__(self, config, params):
+        super().__init__(config)
+
+        # string representation
+        self.str_repr = (
+            f"tcvae"
+            f"_L{config['latent_dim']}"
+            f"_beta{params['beta']}"
+            f"_wseed{config['weight_seed']}"
+        )
+
+        # Add Loss / Optimizer
+        self.vae.add_loss(self.tcvae_loss(config, params))
+        self.vae.compile(optimizer=add_optimizer(config))
+
+    def tcvae_loss(self, config, params):
+        """TCVAE Batch Loss:
+        KL divergence + reconstruction loss + beta * total correlation
+        """
+
+        kl = kl_divergence(self.mean, self.log_var)
+
+        reconstruction_loss = get_reconstruction_loss(
+            self._in,
+            self._out,
+            config,
+        )
+
+        tc = total_correlation(self._z, self.mean, self.log_var)
+
+        return kl + reconstruction_loss + (params["beta"] - 1) * tc
+
+
+class DIPVAE(BaseVAE):
+    def __init__(self, config, params):
+        super().__init__(config)
+
+        # string representation
+        self.str_repr = (
+            f"dipvae"
+            f"_type{params['dip_vae_type']}"
+            f"_L{config['latent_dim']}"
+            f"_loffdiag{params['lambda_off_diag']}"
+            f"_ldiag{params['lambda_diag']}"
+            f"_wseed{config['weight_seed']}"
+        )
+
+        # Add Loss / Optimizer
+        self.vae.add_loss(self.dip_vae_loss(config, params))
+        self.vae.compile(optimizer=add_optimizer(config))
+
+    def dip_vae_loss(self, config, params):
+        """DIPVAE Batch Loss:
+        KL divergence + reconstruction loss + DIP regularizer
+        """
+        kl = kl_divergence(self.mean, self.log_var)
+
+        reconstruction_loss = get_reconstruction_loss(
+            self._in,
+            self._out,
+            config,
+        )
+
+        # DIP VAE REGULARIZATION
+        if params["dip_vae_type"] == "i":
+            cov_matrix = compute_covariance_mean(self.mean)
+        elif params["dip_vae_type"] == "ii":
+            cov_matrix = compute_covariance_z(self.mean, self.log_var)
+        else:
+            raise ValueError(
+                f"Unknown DIPVAE type {params['dip_vae_type']}."
+                "Choose either 'i' or 'ii'."
+            )
+
+        regularizer = dip_vae_regularizer(
+            cov_matrix,
+            params["lambda_off_diag"],
+            params["lambda_diag"],
+        )
+
+        return kl + reconstruction_loss + regularizer
